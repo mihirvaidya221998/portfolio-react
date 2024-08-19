a3409da2b09d6cb6a3567a3c6489e20edb6004ae
Mihir Vaidya <mihirsv22@gmail.com>
2024-08-18 21:17:52 -0500

    Improved the About Me Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import aboutImg from '../assets/projects/IMG_3356.jpeg'
 import { ABOUT_TEXT } from '../constants';
 import { motion } from 'framer-motion';
 
 function About() {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-      <h2 className='my-20 text-center text-4xl'>About Me</h2>
-      <div className="flex flex-wrap">
-        <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:0.5}} className="w-full lg:w-1/2 lg:p-8">
-            <div className="flex items-center justify-center">
-                <img className='rounded-2xl w-64 h-64 lg:w-96 lg:h-96' src={aboutImg} alt="" />
-            </div>
+      <div className='flex flex-col items-center justify-center my-20'>
+        <h2 className='text-center text-4xl mb-8'>About Me</h2>
+        <motion.div
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 50 }}
+          transition={{ duration: 0.5 }}
+          className='max-w-3xl'
+        >
+          <p className='text-center my-2 py-6'>{ABOUT_TEXT}</p>
         </motion.div>
-        <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:0.5}} className='w-full lg:w-1/2'>
-                <div className="flex justify-center lg:justify-start">
-                    <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
-                </div>
-            </motion.div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
